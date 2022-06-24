171b75a542ca026bf7fd4f2c1311196f80af914d
OTKRyu <appn12@gmail.com>
2022-06-24 15:51:25 +0900

    feat: add functions for status

--- src/components/Status/Status.tsx
+++ src/components/Status/Status.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { resourceLimits } from 'worker_threads';
 
 type user = {
   dailyCheck: Array<boolean>;
@@ -9,4 +10,21 @@ type userProps = {
   user: user;
 };
 
-function Status(props: userProps) {}
+function Status(props: userProps) {
+  function arraySum(numbers: Array<number>) {
+    const result = numbers.reduce((prev, cur) => {
+      return prev + cur;
+    }, 0);
+    return result;
+  }
+
+  function arrayCount(bools: Array<boolean>) {
+    const result = bools.filter((bool) => {
+      if (bool === true) {
+        return true;
+      }
+      return false;
+    });
+    return result.length;
+  }
+}
