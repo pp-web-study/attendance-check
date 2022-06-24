b89b080eea5604a2f5c3b0cad3ff8b8fcf08e2d6
OTKRyu <appn12@gmail.com>
2022-06-24 14:46:22 +0900

    feat: add type for status

--- src/components/Status/Status.tsx
+++ src/components/Status/Status.tsx
@@ -1,1 +1,12 @@
-export {};
+import React from 'react';
+
+type user = {
+  dailyCheck: Array<boolean>;
+  dailyCoin: Array<number>;
+};
+
+type userProps = {
+  user: user;
+};
+
+function Status(props: userProps) {}
