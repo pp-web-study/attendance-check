5b3c4889850395bf6cd537d360ecbb6f70b6b126
OTKRyu <appn12@gmail.com>
2022-06-27 16:44:31 +0900

    feat: add rate and add basic html

--- src/components/Status/Status.tsx
+++ src/components/Status/Status.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { resourceLimits } from 'worker_threads';
+import React, { useState } from 'react';
 
 type user = {
   dailyCheck: Array<boolean>;
@@ -27,4 +26,28 @@ function Status(props: userProps) {
     });
     return result.length;
   }
+
+  const [rate, setRate] = useState(0);
+
+  return (
+    <div className="wrapper">
+      <div className="row">
+        <div className="unit"></div>
+        <div className="rate">{rate}</div>
+      </div>
+      <div className="row">
+        <div className="box">
+          <div className="count">{arrayCount(props.user.dailyCheck)}</div>
+          <div className="division"></div>
+          <div className="sum">{arraySum(props.user.dailyCoin)}</div>
+        </div>
+      </div>
+      <div className="row">
+        <span>
+          <i></i>당첨되신 PCI는 <em>24시간 이내 지급되며,</em> 즉시 사용이
+          가능합니다.
+        </span>
+      </div>
+    </div>
+  );
 }
