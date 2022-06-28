9064fa3ee129169b7f96bcbe6883e0e068f28d5c
eun-zi <rladmswlek@naver.com>
2022-06-28 10:38:45 +0900

    feat: 달력 위,아래 선 추가

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className="line"></div>
       <Calendar />
+      <div className="line"></div>
     </div>
   );
 }
