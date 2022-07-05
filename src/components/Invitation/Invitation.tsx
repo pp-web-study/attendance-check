dde44c17e8aaa0ef4f216e5c97f6daa8c78e5ade
OTKRyu <appn12@gmail.com>
2022-07-05 17:34:10 +0900

    feat: add basic html for invitation

--- src/components/Invitation/Invitation.tsx
+++ src/components/Invitation/Invitation.tsx
@@ -1,1 +1,26 @@
-export {};
+import React, { useState } from 'react';
+
+function Invitation() {
+  const goInviteEvent = function () {
+    console.log('move to invitation event page');
+  };
+
+  return (
+    <div onClick={goInviteEvent}>
+      <div>
+        <div>
+          <span>PCI 더 받고 싶다면</span>
+          <br />
+          <em>친구추천 하고 PCI 받으세요!</em>
+          <br />
+          <span>친구 추천하기&gt;</span>
+        </div>
+      </div>
+      <div>
+        <img src="" alt="" />
+      </div>
+    </div>
+  );
+}
+
+export default Invitation;
