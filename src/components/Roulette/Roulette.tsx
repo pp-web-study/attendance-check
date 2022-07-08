9e81583e60cc0b38f3850dd0800725b1625f0532
OTKRyu <appn12@gmail.com>
2022-07-08 15:12:54 +0900

    feat: add basic roulette

--- src/components/Roulette/Roulette.tsx
+++ src/components/Roulette/Roulette.tsx
@@ -1,1 +1,24 @@
-export {};
+import React, { useState } from 'react';
+import { Wheel } from 'react-custom-roulette';
+
+function Roulette() {
+  const [data, setData] = useState([
+    { option: '0', style: { backgroundColor: 'blue', textColor: 'black' } },
+    { option: '1', style: { backgroundColor: 'white', textColor: 'black' } },
+    { option: '2' },
+  ]);
+
+  return (
+    <div>
+      <Wheel
+        mustStartSpinning={true}
+        prizeNumber={1}
+        data={data}
+        backgroundColors={['#3e3e3e', '#df3428']}
+        textColors={['#ffffff']}
+      ></Wheel>
+    </div>
+  );
+}
+
+export default Roulette;
