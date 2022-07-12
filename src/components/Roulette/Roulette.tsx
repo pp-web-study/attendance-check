283111ef6f016764ea57be26ffa17a830f863c96
OTKRyu <appn12@gmail.com>
2022-07-12 17:20:44 +0900

    style: add css and various options

--- src/components/Roulette/Roulette.tsx
+++ src/components/Roulette/Roulette.tsx
@@ -1,15 +1,20 @@
 import React, { useState } from 'react';
 import { Wheel } from 'react-custom-roulette';
+import './Roulette.css';
 
 function Roulette() {
   const [data, setData] = useState([
-    { option: '0', style: { backgroundColor: 'blue', textColor: 'black' } },
-    { option: '1', style: { backgroundColor: 'white', textColor: 'black' } },
-    { option: '2' },
+    { option: '0', style: { backgroundColor: 'red', textColor: 'black' } },
+    { option: '1', style: { backgroundColor: 'orange', textColor: 'black' } },
+    { option: '2', style: { backgroundColor: 'yellow', textColor: 'black' } },
+    { option: '3', style: { backgroundColor: 'green', textColor: 'black' } },
+    { option: '4', style: { backgroundColor: 'blue', textColor: 'black' } },
+    { option: '5', style: { backgroundColor: 'indigo', textColor: 'black' } },
+    { option: '6', style: { backgroundColor: 'purple', textColor: 'black' } },
   ]);
 
   return (
-    <div>
+    <div className="roulette-box">
       <Wheel
         mustStartSpinning={true}
         prizeNumber={1}
@@ -17,6 +22,7 @@ function Roulette() {
         backgroundColors={['#3e3e3e', '#df3428']}
         textColors={['#ffffff']}
       ></Wheel>
+      <button></button>
     </div>
   );
 }
