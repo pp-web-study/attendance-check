cbc8536bc21c7a3be2ddfdc8bd6980383dc52a71
OTKRyu <appn12@gmail.com>
2022-07-14 14:55:34 +0900

    feat: split modal component to before and already
    move modal.css to PopMsg.css
    make already and before components

--- src/components/Roulette/components/Modal.tsx
+++ src/components/Roulette/components/Modal.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { solid } from '@fortawesome/fontawesome-svg-core/import.macro';
+import React, { useState } from 'react';
 
+import Already from './Already';
+import Before from './Before';
 import './Modal.css';
 
 interface ModalDefaultType {
@@ -9,11 +9,17 @@ interface ModalDefaultType {
   prizeNumber: number;
 }
 
-const Modal = ({ onClickModal, prizeNumber }: ModalDefaultType) => {
+function Modal({ onClickModal, prizeNumber }: ModalDefaultType) {
   const popClose = () => {
     if (onClickModal) {
       onClickModal();
     }
+  };
+
+  const [isAlready, setIsAlready] = useState(false);
+
+  const changeCheckStatus = function () {
+    setIsAlready(!isAlready);
   };
 
   return (
@@ -22,33 +28,20 @@ const Modal = ({ onClickModal, prizeNumber }: ModalDefaultType) => {
       <div className="white-background">
         <div className="position">
           <div className="modal-content">
-            <div className="row imog">
-              <FontAwesomeIcon icon={solid('hands-clapping')}></FontAwesomeIcon>
-            </div>
-            <div className="row">
-              오늘은 벌써 출첵 하셨네요! <br />
-              내일 다시 만나요! ^_^
-            </div>
-            <div className="row">
-              <div className="blue-box">
-                <em className="tip">PCI 모오는 꿀팁★</em>
-                <br />
-                페이코인에서 게임하고
-                <br />
-                PCI 받아가세요!
-              </div>
-            </div>
+            {isAlready ? <Already /> : <Before prizeNumber={prizeNumber} />}
             <div className="row">
               <button className="close-btn" onClick={popClose}>
                 닫기
               </button>
-              <button className="game-btn">게임하기</button>
+              <button className="game-btn" onClick={changeCheckStatus}>
+                게임하기
+              </button>
             </div>
           </div>
         </div>
       </div>
     </div>
   );
-};
+}
 
 export default Modal;
